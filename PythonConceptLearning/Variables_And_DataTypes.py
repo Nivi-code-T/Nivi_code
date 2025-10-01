36158e4440af0f452beee903a66535bb7dcda2fa
Niv T <niveteggi@gmail.com>
2025-09-30 22:12:32 -0500

    Added Python Learning Concepts Practice Codes

---
+++ PythonConceptLearning/Variables_And_DataTypes.py
@@ -0,0 +1,27 @@
+#Create variable
+age=30
+name="Python"
+
+#Print DataType of the variable
+print(type(age))
+print(type(name))
+
+#Convert Variable to different data type
+age_float=float(age)
+print(age_float)
+
+#Integer to String
+int1=15
+str=str(int1)
+print(str)
+
+#string to bool
+str_bool_true=bool('hello')
+str_bool_false=bool('')
+print(f"{str_bool_true}")
+print(f"{str_bool_false}")
+
+#float to integer
+float=15.55
+float_int=int(float)
+print(float_int)
