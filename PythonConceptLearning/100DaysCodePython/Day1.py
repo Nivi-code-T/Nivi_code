bc4e71c4510abcd2cdcc4ca2bb69e8b046597aec
Niv T <niveteggi@gmail.com>
2025-11-19 14:40:59 -0600

    Added Python Day1 Code Practice and Project

---
+++ PythonConceptLearning/100DaysCodePython/Day1.py
@@ -0,0 +1,22 @@
+# print("Hello World!")
+# print("1. Mix 500g of Flour, 10g Yeast and 300ml Water in a bowl.")
+# print("2. Knead the dough for 10 minutes.")
+# print("3. Add 3g of Salt.")
+# print("4. Leave to rise for 2 hours.")
+# print("5. Bake at 200 degrees C for 30 minutes.")
+#
+# #Concatenation String
+# print("Hello" + " " + "Nive")
+#
+# #This lIne of code will take an input using the input() function
+# print("Hello" + " " + input("What is your Name"))
+# print("Hello" + " " + input("What is your Name") + "!")
+#
+# #pythonVariables
+# name=input("What is your name")
+# print(name)
+
+#String Length using len() function
+name="Nive"
+print(len(name))
+
---
+++ PythonConceptLearning/100DaysCodePython/Poject1.py
@@ -0,0 +1,5 @@
+#Brand Name generator Project
+print("Welcome to the brand name generator")
+name_of_city=input("Enter name of the city?\n")
+name_of_pet=input("Enter name of the pet?\n")
+print("Brand Name is : " + name_of_city + " " + name_of_pet)
