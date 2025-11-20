e01cd265920a5898729ca319476a3f5058e65003
Niv T <niveteggi@gmail.com>
2025-11-19 21:00:01 -0600

    Modified Day2 Practice code

--- PythonConceptLearning/100DaysCodePython/Day2Project.py
+++ PythonConceptLearning/100DaysCodePython/Day2Project.py
@@ -5,3 +5,10 @@ tip=int(input("what % tip would you like to give ? 10 12 15"))
 people=int(input("How many people to split the bill"))
 bill_with_tip=bill + bill * (tip/100)
 print(bill_with_tip)
+
+#Flight ticket calculator
+print("Welcome to flight calculator")
+flight_bill=int(input("Enter Ticket price"))
+flight_offer=int(input("Enter the % off"))
+flight_pricer_after_off=flight_bill - flight_bill (flight_offer/100)
+print(flight_pricer_after_off)
